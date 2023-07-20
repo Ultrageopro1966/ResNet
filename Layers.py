16d6734dec40216ab226744d6601562e7eecedb6
UltraGeoPro <120571667+Ultrageopro1966@users.noreply.github.com>
2023-07-20 17:44:44 +0300

    Update Layers.py

--- Layers.py
+++ Layers.py
@@ -1,64 +1,64 @@
 from keras import layers
 
 
-# Определение класса ResidualConvBlock
+# Definition of the ResidualConvBlock class
 class ResidualConvBlock(layers.Layer):
     def __init__(self, filters):
         super().__init__()
 
-        # Слой свертки 1x1 для пропуска соединения (skip connection)
+        # 1x1 Convolution layer for skip connection
         self.x_scip = layers.Conv2D(filters, 1, (2, 2), padding="same")
 
-        # Слой свертки 3x3
+        # 3x3 Convolution layer
         self.conv1 = layers.Conv2D(filters, 3, (2, 2), padding="same")
         self.norm1 = layers.BatchNormalization()
         self.act1 = layers.ReLU()
 
-        # Слой свертки 3x3
+        # 3x3 Convolution layer
         self.conv2 = layers.Conv2D(filters, 3, (1, 1), padding="same")
         self.norm2 = layers.BatchNormalization()
 
-        # Операция сложения
+        # Addition operation
         self.add = layers.Add()
         self.act2 = layers.ReLU()
 
     def call(self, inputs):
         x_scip = self.x_scip(inputs)
 
-        # Прямой проход через блок
+        # Forward pass through the block
         x = self.conv1(inputs)
         x = self.norm1(x)
         x = self.act1(x)
         x = self.conv2(x)
         x = self.norm2(x)
-        x = self.add([x_scip, x])  # Пропуск соединения (skip connection)
+        x = self.add([x_scip, x])  # Skip connection
         return self.act2(x)
 
 
-# Определение класса ResidualIdentitiBlock
+# Definition of the ResidualIdentitiBlock class
 class ResidualIdentitiBlock(layers.Layer):
     def __init__(self, filters):
         super().__init__()
 
-        # Слой свертки 3x3
+        # 3x3 Convolution layer
         self.conv1 = layers.Conv2D(filters, 3, padding="same")
         self.norm1 = layers.BatchNormalization()
         self.act1 = layers.ReLU()
 
-        # Слой свертки 3x3
+        # 3x3 Convolution layer
         self.conv2 = layers.Conv2D(filters, 3, padding="same")
         self.norm2 = layers.BatchNormalization()
 
-        # Операция сложения
+        # Addition operation
         self.add = layers.Add()
         self.act2 = layers.ReLU()
 
     def call(self, inputs):
-        # Прямой проход через блок
+        # Forward pass through the block
         x = self.conv1(inputs)
         x = self.norm1(x)
         x = self.act1(x)
         x = self.conv2(x)
         x = self.norm2(x)
-        x = self.add([inputs, x])  # Пропуск соединения (skip connection)
+        x = self.add([inputs, x])  # Skip connection
         return self.act2(x)
